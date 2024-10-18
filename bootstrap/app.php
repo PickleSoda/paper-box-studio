5014de7fc9cf0a247dce5e3db0cb3c66738443d4
AchiUgulava <achiugulava@gmail.com>
2024-10-18 19:32:19 +0400

    fix: session timeout redirect locale bug

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -22,6 +23,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
             'locale' => \App\Http\Middleware\SetLocale::class,
         ]);
+        $middleware->redirectGuestsTo(fn (Request $request) => route('login', app()->getLocale()));
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
